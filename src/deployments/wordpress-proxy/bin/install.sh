cf64595719b2e186c9a295ad287533f156edfd29
InvisiBug <matthewjkavanagh@gmail.com>
2025-02-10 23:54:25 +0000

    added wordpress proxy

---
+++ src/deployments/wordpress-proxy/bin/install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+helm install wordpress-proxy . --namespace wordpress-proxy --create-namespace
---
+++ src/deployments/wordpress-proxy/bin/upgrade.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+helm upgrade wordpress-proxy . --namespace wordpress-proxy --create-namespace
